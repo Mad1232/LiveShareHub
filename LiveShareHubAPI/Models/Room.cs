7a55578b11e417237fd02b8596369a362bb69996
Prakarsha <musicstyle1232@gmail.com>
2025-05-30 17:40:43 -0500

    Finished code for Room.cs and SharedFile.cs

--- LiveShareHubAPI/Models/Room.cs
+++ LiveShareHubAPI/Models/Room.cs
@@ -1,41 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+//Room has an roomID, list of Files it holds, createdAt to auto delete rooms after 24 hours.
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+public class Room{
 
-var app = builder.Build();
+    public string roomID {get; set;} //getters and setters
+    public DateTime createdAt {get; set;}
+    public List<SharedFile> Files {get; set;}
+    
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+    //constructor
+    public Room(String roomID){
+        this.roomID = roomID;
+        createdAt = DateTime.UtcNow;
+        Files = new List<SharedFile>(); 
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
 }
